8770bc9ffa88f0e9170af947b76e0f43baa1106f
TitoNadar <titonadar06@gmail.com>
2018-04-21 07:02:01 +0530

    helper  fonts added

---
+++ app/src/main/java/com/example/tito/engage/HelperFonts/MyTextView.java
@@ -0,0 +1,36 @@
+package com.example.tito.engage.HelperFonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by one on 3/12/15.
+ */
+public class MyTextView extends TextView{
+
+    public MyTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/lato_regular.ttf");
+            setTypeface(tf);
+        }
+    }
+
+}

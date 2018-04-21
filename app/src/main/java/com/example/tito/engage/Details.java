d2b1abea23dd634c704637b42d02708273fc7910
TitoNadar <titonadar06@gmail.com>
2018-04-21 10:57:45 +0530

    notification  setup

--- app/src/main/java/com/example/tito/engage/Details.java
+++ app/src/main/java/com/example/tito/engage/Details.java
@@ -6,17 +6,25 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Details extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+button=findViewById(R.id.button_register);
 
-
-
+button.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
         showNotification();
+    }
+});
+
+
     }
 
     private void showNotification() {

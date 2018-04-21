828886e787668f11e60f18185b4131c49531bac7
TitoNadar <titonadar06@gmail.com>
2018-04-21 10:56:10 +0530

    xml od detailed activity

--- app/src/main/java/com/example/tito/engage/Details.java
+++ app/src/main/java/com/example/tito/engage/Details.java
@@ -1,5 +1,9 @@
 package com.example.tito.engage;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,31 @@ public class Details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+
+
+        showNotification();
+    }
+
+    private void showNotification() {
+        Intent intent = new Intent(this, MainActivity.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("You have been Registered")
+                .setContentText("An email will be sent to you shortly")
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+               .build();
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, n);
     }
 }
